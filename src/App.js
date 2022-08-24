ddb05bd8a902ec55a6caaae2fc07debd233694bb
Betul Zemheri <a.betul2200@gmail.com>
2022-08-24 19:56:14 +0300

    fire contact

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import Contacts from './components/contacts/Contacts';
+import FormComponent from './components/form/FormComponent';
+import {useState} from "react"
+import {AddUser,UpdateUser} from "./utils/functions"
+import {ToastContainer} from "react-toastify"
+
+const initialValues={username:"",phoneNumber:"",gender:"NO INFO"}
 
 function App() {
+  const [info,setInfo]=useState(initialValues)
+  const [isAdd,setIsAdd]=useState("ADD")
+ 
+  
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    if(info.id){
+      UpdateUser(info)
+    }
+    else{
+      AddUser(info)
+    }
+    setInfo(initialValues);
+    setIsAdd("ADD")
+  }
+
+  const editUser=(id,username,phoneNumber,gender)=>{
+    setIsAdd("UPDATE")
+    setInfo({id,username,phoneNumber,gender})
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FormComponent info={info} setInfo={setInfo} handleSubmit={handleSubmit} isAdd={isAdd} />
+      <Contacts editUser={editUser}/>
+      <ToastContainer/>
     </div>
   );
 }
